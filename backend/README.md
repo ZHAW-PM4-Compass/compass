724cc79052e383392746a4f1bdb07eecc1a81a28
elhaykar <elhaykar@students.zhaw.ch>
2024-03-15 12:03:55 +0100

    commit BE

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+# Compass Backend Applikation
+
+Das Backend von Compass ist als eine "Spring Boot mit Java SE" Applikation umgesetzt.
+
+# Requirements
+- JDK 21
+- Gradle 8.1.1+
---
+++ backend/src/test/java/ch/zhaw/pm4/compass/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.pm4.compass.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

01c74caa10ab5bb8f619e5d6997ed952b67e34ff
R44CX <29802889+r44cx@users.noreply.github.com>
2024-03-22 15:04:24 +0100

    add current state of deployment and backend docker file

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 # This Script will start front and backend inside the docker container
+
+# go to /backend and run grade run
+cd /backend
+gradle run &
+
+# go to /frontend and run npm run start
+cd /frontend
+npm run start

05a673da673c2e54876a721cd156763b1a9b5e71
noah <35225015+noahbaumgartner@users.noreply.github.com>
2024-03-25 20:30:14 +0100

    Revert "began with auth0"
    This reverts commit 73f50618a50144f84acdf640486e9ef51f6843c5.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,14 @@
-import { UserProvider } from '@auth0/nextjs-auth0/client';
-
 export default function RootLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode
 }) {
   return (
-    <UserProvider>
       <section>
         {/* Include shared UI here e.g. a header or sidebar */}
-        <nav><a className="mr-4" href={"/"}>Startseite</a><a href={"/time-management"}>Zeit Erfassung</a></nav>
+        <nav><a href={"/"}>Startseite</a><a href={"/time-management"}>Zeit Erfassung</a></nav>
+
         {children}
       </section>
-    </UserProvider>
   )
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,3 @@
-'use client';
-import { useUser } from '@auth0/nextjs-auth0/client';
-
 export default function Page() {
-    const { user, isLoading, error } = useUser();
-
-    return (
-        <div>
-            <h1>Hello, Welcome to Compass</h1>
-            <div>
-              <button>
-                {user ? (
-                  <a href="/api/auth/logout">Logout</a>
-                ) : (
-                  <a href="/api/auth/login?returnTo=/profile">Login</a>
-                )}
-              </button>
-                <p>User: {JSON.stringify(user)}</p>
-                <p>is Loading: {JSON.stringify(isLoading)}</p>
-                <p>Error: {JSON.stringify(error)}</p>
-            </div>
-        </div>
-    )
+    return <h1>Hello, Welcome to Compass</h1>
 }

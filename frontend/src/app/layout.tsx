73f50618a50144f84acdf640486e9ef51f6843c5
noah <35225015+noahbaumgartner@users.noreply.github.com>
2024-03-25 20:29:07 +0100

    began with auth0

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,17 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
 export default function RootLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode
 }) {
   return (
+    <UserProvider>
       <section>
         {/* Include shared UI here e.g. a header or sidebar */}
-        <nav><a href={"/"}>Startseite</a><a href={"/time-management"}>Zeit Erfassung</a></nav>
-
+        <nav><a className="mr-4" href={"/"}>Startseite</a><a href={"/time-management"}>Zeit Erfassung</a></nav>
         {children}
       </section>
+    </UserProvider>
   )
 }

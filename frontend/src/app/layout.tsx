3b2cb8a3eea61566fca0e4d89258bbab00a89989
noah <35225015+noahbaumgartner@users.noreply.github.com>
2024-03-29 21:51:08 +0100

    added back tailwind

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Compass - Home",

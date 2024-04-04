245e8e44b3a1ec4509eb69cbf9dfba1a95b96074
noah <35225015+noahbaumgartner@users.noreply.github.com>
2024-04-04 23:06:50 +0200

    simplified backend call

--- frontend/src/actions/workingHours.tsx
+++ frontend/src/actions/workingHours.tsx
@@ -1,20 +1,10 @@
 "use server";
 
-import { getSession } from "@auth0/nextjs-auth0";
-import { Configuration, DaySheetControllerApi, CreateDaySheetDto } from "@/api/compassClient";
+import { getDaySheetControllerApi } from "@/api/connector";
+import { CreateDaySheetDto } from "@/api/compassClient";
 
 export async function createDaySheet(formData: FormData) {
-    const session = await getSession();
-    const config = new Configuration({
-        basePath: process.env.REACT_APP_API_BASE_PATH,
-        baseOptions: {
-            headers: {
-                Authorization: `Bearer ${session?.accessToken}`,
-            }
-        }
-    });
-
-    const daySheetApi = new DaySheetControllerApi(config);
+    const daySheetApi = await getDaySheetControllerApi();
     const createDaySheetDto: CreateDaySheetDto = {
         date: formData.get("date") as string,
         day_report: formData.get("dayReport") as string,

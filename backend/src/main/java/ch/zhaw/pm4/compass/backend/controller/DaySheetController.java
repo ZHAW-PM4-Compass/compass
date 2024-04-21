c88d1a866c6f43743729ec00b2a3a78ab786e043
brunndar <brunndar@students.zhaw.ch>
2024-04-21 15:37:54 +0200

    #17 fixed imports in controller

--- backend/src/main/java/ch/zhaw/pm4/compass/backend/controller/DaySheetController.java
+++ backend/src/main/java/ch/zhaw/pm4/compass/backend/controller/DaySheetController.java
@@ -3,7 +3,9 @@ package ch.zhaw.pm4.compass.backend.controller;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
+import ch.zhaw.pm4.compass.backend.model.dto.WorkHourDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;

ec4de5f7a6c3efdc1b4daa333849d3c81ea5d9d1
elhaykar <elhaykar@students.zhaw.ch>
2024-04-21 22:02:37 +0200

    make swagger ui available based on env var and not subject to authentication #59

--- backend/src/main/java/ch/zhaw/pm4/compass/backend/security/SecurityConfig.java
+++ backend/src/main/java/ch/zhaw/pm4/compass/backend/security/SecurityConfig.java
@@ -1,26 +1,28 @@
 package ch.zhaw.pm4.compass.backend.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-          .authorizeHttpRequests((authorize) -> authorize
-            .anyRequest().authenticated()
-          )
-          .oauth2ResourceServer(oauth2 -> oauth2
-            .jwt(withDefaults())
-          )
-          .build();
-    }
+	@Value("${springdoc.api-docs.enabled}")
+	private Boolean swaggerOn;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests((authorize) -> {
+			if (swaggerOn) {
+				authorize.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll();
+			}
+			authorize.anyRequest().authenticated();
+		}).oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults())).build();
+	}
 }

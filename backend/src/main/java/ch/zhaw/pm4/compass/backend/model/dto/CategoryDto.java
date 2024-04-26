10867413a92de946df2fd81964a231bfdf7899fe
elhaykar <elhaykar@students.zhaw.ch>
2024-04-26 19:35:35 +0200

    clean unused resources directory

--- backend/src/main/java/ch/zhaw/pm4/compass/backend/model/dto/CategoryDto.java
+++ backend/src/main/java/ch/zhaw/pm4/compass/backend/model/dto/CategoryDto.java
@@ -5,10 +5,14 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class CategoryDto {
 	@NonNull

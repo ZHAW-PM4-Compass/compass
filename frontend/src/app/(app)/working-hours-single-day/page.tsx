a377bc932eaa23c8171e70e1d3ab787e496b6b67
brunndar <brunndar@students.zhaw.ch>
2024-04-28 18:51:15 +0200

    #16 Implemented logic to select a single DaySheet and edit all the timestamps from it

---
+++ frontend/src/app/(app)/working-hours-single-day/page.tsx
@@ -0,0 +1,156 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import Table from "@/components/table";
+import {
+    Delete24Regular,
+    Edit24Regular,
+    Save24Regular
+} from "@fluentui/react-icons";
+import Title1 from "@/components/title1";
+import Button from "@/components/button";
+import {GetDaySheetDto, GetTimestampDto, ParticipantDto, UserDto, WorkHourDto} from "@/openapi/compassClient";
+import {toast} from "react-hot-toast";
+import Modal from "@/components/modal";
+import Input from "@/components/input";
+
+function TimestampUpdateModal({ close, onSave, timestamp }: Readonly<{
+    close: () => void;
+    onSave: () => void;
+    timestamp: GetTimestampDto | undefined;
+}>) {
+    const onSubmit = (formData: FormData) => {
+        const getTimestampDto: GetTimestampDto = {
+            id: formData.get("id") as any as number,
+            day_sheet_id: formData.get("day_sheet_id") as any as number,
+            start_time: formData.get("start_time") as string,
+            end_time: formData.get("end_time") as string,
+        };
+
+        toast.success('Updated timestamp');
+    }
+
+    return (
+        <Modal
+            title="Zeit bearbeiten"
+            footerActions={
+                <Button Icon={Save24Regular} type="submit">Speichern</Button>
+            }
+            close={close}
+            onSubmit={onSubmit}
+        >
+            <Input type="text" placeholder="Startuhrzeit" className="mb-4 mr-4 w-48 inline-block" name="given_name" required={true} value={timestamp?.start_time} />
+            <Input type="text" placeholder="Enduhrzeit" className="mb-4 mr-4 w-48 inline-block" name="family_name" required={true} value={timestamp?.end_time} />
+        </Modal>
+    );
+}
+
+const DaySheetViewSingleDay: React.FC = () => {
+    const router = useRouter();
+    const [currentDate, setCurrentDate] = useState<Date | undefined>();
+    const [selectedTimestamp, setSelectedTimestamp] = useState<GetTimestampDto>();
+    const [timestamps, setTimestamps] = useState<GetTimestampDto[]>([]);
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
+
+    const mockdata: GetTimestampDto[] = [
+        { id: 0, day_sheet_id: 0, start_time: "08:00", end_time: "17:00" } as GetTimestampDto,
+        { id: 1, day_sheet_id: 1, start_time: "08:15", end_time: "17:55" } as GetTimestampDto,
+        { id: 2, day_sheet_id: 2, start_time: "08:20", end_time: "17:05" } as GetTimestampDto,
+        { id: 3, day_sheet_id: 3, start_time: "08:45", end_time: "17:00" } as GetTimestampDto,
+    ];
+
+    let initLoad = false;
+    useEffect(() => {
+        if (!initLoad) {
+            initLoad = true;
+            const queryParams = new URLSearchParams(window.location.search);
+            const dateString = queryParams.get('date');
+
+            // If the date parameter exists in the query string, parse it and set it to the state
+            if (dateString) {
+                const parsedDate = new Date(dateString);
+                setCurrentDate(parsedDate);
+                getDaysheet(parsedDate).then((result) => {
+                    setTimestamps(result.timestamps);
+                    toast.success('Done fetching timestamps');
+                }).catch(() => {
+                    setTimestamps(mockdata);
+                    toast.error('Error fetching timestamps, using mocked data');
+                });
+            }
+        }
+    }, []);
+
+    const getDaysheet = async (date: Date) => {
+        try {
+            // Make a GET request using the fetch API
+            const response = await fetch(`http://localhost:8080/api/daysheet/getByDate/${date}`);
+
+            // Check if the response is successful (status code 200)
+            if (!response.ok) {
+                // Handle non-successful response (e.g., throw an error)
+                throw new Error(`Failed to fetch data`);
+            }
+
+            // Parse the response body as JSON
+            // Return the data
+            return await response.json();
+        } catch (error) {
+            // Handle any errors (e.g., log the error)
+            console.error('Error fetching data:', error);
+            // rethrow the error to let the caller handle it
+            throw error;
+        }
+    };
+    
+    return (
+        <div>
+            {showUpdateModal && (
+                <TimestampUpdateModal
+                    close={() => setShowUpdateModal(false)}
+                    onSave={() => {if (currentDate) getDaysheet(currentDate)}}
+                    timestamp={selectedTimestamp} />
+            )}
+            <div className="flex flex-col sm:flex-row justify-between">
+                <Title1>Kontrolle Arbeitszeit</Title1>
+                <div className="mt-2 sm:mt-0 font-bold">
+                    {currentDate && <p>{currentDate.toDateString()}</p>}
+                </div>
+            </div>
+            <Table
+                data={timestamps}
+                columns={[
+                    {
+                        header: "Startuhrzeit",
+                        title: "start_time"
+                    },
+                    {
+                        header: "Enduhrzeit",
+                        title: "end_time"
+                    },
+                    {
+                        header: "Dauer",
+                        title: "start_time - end_time"
+                    }
+                ]}
+                actions={[
+                    {
+                        icon: Delete24Regular,
+                        label: "Löschen",
+                        onClick: (id) => {}
+                    },
+                    {
+                        icon: Edit24Regular,
+                        onClick: (id) => {
+                            setSelectedTimestamp(timestamps[id]);
+                            setShowUpdateModal(true);
+                        }
+                    }
+                ]}>
+
+            </Table>
+        </div>
+    );
+};
+
+export default DaySheetViewSingleDay;

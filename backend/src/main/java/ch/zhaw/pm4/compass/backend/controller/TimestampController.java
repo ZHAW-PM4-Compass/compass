fe22f74bb06e64c85bc2000809d25c155c55fd54
elhaykar <elhaykar@students.zhaw.ch>
2024-05-12 18:16:46 +0200

    fix tests #96

--- backend/src/main/java/ch/zhaw/pm4/compass/backend/controller/TimestampController.java
+++ backend/src/main/java/ch/zhaw/pm4/compass/backend/controller/TimestampController.java
@@ -27,6 +27,7 @@ public class TimestampController {
 	@PostMapping(produces = "application/json")
 	public ResponseEntity<TimestampDto> createTimestamp(@RequestBody TimestampDto timestamp,
 			Authentication authentication) {
+		System.out.println(timestamp.toString());
 		if (!timestamp.verifyTimeStamp()) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}

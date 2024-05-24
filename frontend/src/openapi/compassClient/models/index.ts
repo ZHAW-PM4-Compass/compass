d41323a866204d69b02403fcb7e1ab08ae4ba934
r44cx <29802889+r44cx@users.noreply.github.com>
2024-05-24 15:32:02 +0200

    uopdate compassClient #23

--- frontend/src/openapi/compassClient/models/index.ts
+++ frontend/src/openapi/compassClient/models/index.ts
@@ -4,6 +4,7 @@ export * from './AuthZeroUserDto';
 export * from './CategoryDto';
 export * from './CreateAuthZeroUserDto';
 export * from './DaySheetDto';
+export * from './ExtendedRatingDto';
 export * from './IncidentDto';
 export * from './RatingDto';
 export * from './SystemStatusDto';

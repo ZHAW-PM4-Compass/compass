e6b7288357cd9081df291a8b16495bf9b11d8240
elhaykar <elhaykar@students.zhaw.ch>
2024-05-27 01:28:17 +0200

    test: add som DaySheet model tests

--- backend/src/test/java/ch/zhaw/pm4/compass/backend/controller/RatingControllerTest.java
+++ backend/src/test/java/ch/zhaw/pm4/compass/backend/controller/RatingControllerTest.java
@@ -1,5 +1,49 @@
 package ch.zhaw.pm4.compass.backend.controller;
 
-public class RatingControllerTest {
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.nimbusds.jose.shaded.gson.Gson;
+import com.nimbusds.jose.shaded.gson.GsonBuilder;
+
+import ch.zhaw.pm4.compass.backend.GsonExclusionStrategy;
+import ch.zhaw.pm4.compass.backend.LocalDateDeserializer;
+import ch.zhaw.pm4.compass.backend.LocalDateSerializer;
+import ch.zhaw.pm4.compass.backend.LocalTimeDeserializer;
+import ch.zhaw.pm4.compass.backend.LocalTimeSerializer;
+import ch.zhaw.pm4.compass.backend.service.RatingService;
+import ch.zhaw.pm4.compass.backend.service.UserService;
+
+public class RatingControllerTest {
+	@Autowired
+	MockMvc mockMvc;
+	@Autowired
+	private WebApplicationContext controller;
+
+	@MockBean
+	private UserService userService;
+
+	@MockBean
+	private RatingService RatingService;
+
+	private Gson gson = new GsonBuilder().registerTypeAdapter(LocalTime.class, new LocalTimeDeserializer())
+			.registerTypeAdapter(LocalTime.class, new LocalTimeSerializer())
+			.registerTypeAdapter(LocalDate.class, new LocalDateDeserializer())
+			.registerTypeAdapter(LocalDate.class, new LocalDateSerializer())
+			.addDeserializationExclusionStrategy(new GsonExclusionStrategy())
+			.addDeserializationExclusionStrategy(new GsonExclusionStrategy()).create();
+
+	@Before()
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(controller).apply(SecurityMockMvcConfigurers.springSecurity())
+				.build();
+	}
 }

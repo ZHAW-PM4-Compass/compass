4f84ae5813a0ac8e063e8390e4aeb3da1cd12de1
r44cx <29802889+r44cx@users.noreply.github.com>
2024-05-31 11:55:51 +0200

    refactor mood page

--- frontend/src/app/(app)/moods/page.tsx
+++ frontend/src/app/(app)/moods/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, type SetStateAction } from "react";
 import Modal from "@/components/modal";
 import Button from "@/components/button";
 import Input from "@/components/input";
@@ -8,16 +8,42 @@ import Select from "@/components/select";
 import Table from "@/components/table";
 import { Edit24Regular, Save24Regular } from "@fluentui/react-icons";
 import Slider from "@/components/slider";
-import { getUserControllerApi, getCategoryControllerApi, getRatingControllerApi, getDaySheetControllerApi } from "@/openapi/connector";
+import {
+  getUserControllerApi,
+  getCategoryControllerApi,
+  getRatingControllerApi,
+  getDaySheetControllerApi,
+} from "@/openapi/connector";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { debounce } from "lodash";
+import toast from "react-hot-toast";
+import {
+  CategoryDto,
+  type DaySheetDto,
+  type RatingDto,
+  type UserDto,
+} from "@/openapi/compassClient";
 
-const allParticipants = "ALL_PARTICIPANTS";
+const MoodModal = ({
+  close,
+  onSave,
+  categories,
+  participants,
+  selectedParticipant,
+  setSelectedParticipant,
+  role,
+}: {
+  close: () => void;
+  onSave: (ratings: any[]) => void;
+  categories: any[];
+  participants: { id: string; label: string }[] | undefined;
+  selectedParticipant: string;
+  setSelectedParticipant: (participant: string) => void;
+  role: string;
+}) => {
+  const [moodValues, setMoodValues] = useState<{ [key: string]: number }>({});
 
-const MoodModal = ({ close, onSave, categories, participants, selectedParticipant, setSelectedParticipant, role }: { close: any, onSave: any, categories: any, participants: any, selectedParticipant: any, setSelectedParticipant: any, role: any }) => {
-  const [moodValues, setMoodValues] = useState({});
-
-  const handleMoodChange = (categoryId, value) => {
+  const handleMoodChange = (categoryId: any, value: number) => {
     setMoodValues((prevValues) => ({
       ...prevValues,
       [String(categoryId)]: value,
@@ -32,6 +58,7 @@ const MoodModal = ({ close, onSave, categories, participants, selectedParticipan
     }));
     console.log("Saving ratings:", ratingsToSave);
     onSave(ratingsToSave);
+    close(); // Close the modal after saving
   };
 
   return (
@@ -44,10 +71,9 @@ const MoodModal = ({ close, onSave, categories, participants, selectedParticipan
               className="mr-4 w-48 inline-block"
               name="participant"
               required
-              data={participants}
+              data={participants || []} // Ensure that `data` is always an array
               value={selectedParticipant}
               onChange={(event) => setSelectedParticipant(event.target.value)}
-              disabled={role === "PARTICIPANT"}
             />
           ) : null}
           <Button Icon={Save24Regular} type="submit" onClick={handleSubmit}>
@@ -67,6 +93,7 @@ const MoodModal = ({ close, onSave, categories, participants, selectedParticipan
               max={category.maximumValue}
               value={moodValues[category.id] || category.minimumValue}
               onChange={(value) => handleMoodChange(category.id, value)}
+              name={""}
             />
           </div>
         ))}
@@ -79,26 +106,78 @@ const MoodTrackingPage = () => {
   const { user } = useUser();
   const [role, setRole] = useState("PARTICIPANT");
   const [showModal, setShowModal] = useState(false);
-  const [ratings, setRatings] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().slice(0, 10));
-  const [participants, setParticipants] = useState([]);
-  const [selectedParticipant, setSelectedParticipant] = useState(user?.sub || "");
-  const [daySheetId, setDaySheetId] = useState(null);
+  const [ratings, setRatings] = useState<RatingDto[]>([]);
+  const [categories, setCategories] = useState<CategoryDto[]>([]);
+  const [selectedDate, setSelectedDate] = useState(
+    new Date().toISOString().slice(0, 10)
+  );
+  const [participants, setParticipants] = useState<UserDto[]>([]);
+  const [selectedParticipant, setSelectedParticipant] = useState(
+    user?.sub || ""
+  );
+  const [daySheetId, setDaySheetId] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchDaySheet = async () => {
       const daySheetApi = getDaySheetControllerApi();
       try {
-        const daySheet = await daySheetApi.getDaySheetDate({ date: selectedDate });
-        setDaySheetId(daySheet?.id);
-        console.log("Fetched daySheetId:", daySheet?.id);
+        if (role === "PARTICIPANT") {
+          const daySheet = await daySheetApi.getDaySheetDate({
+            date: selectedDate,
+          });
+          setDaySheetId(daySheet?.id ?? null); // Provide a default value of null if daySheet?.id is undefined
+          setRatings(daySheet?.moodRatings || []);
+          return;
+        } else {
+          const daySheets =
+            await daySheetApi.getAllDaySheetByParticipantAndMonth({
+              userId: selectedParticipant,
+              month: selectedDate.slice(0, 7),
+            });
+
+          const formattedSelectedDate = new Date(selectedDate.slice(0, 10));
+          console.log("Formatted selected date:", formattedSelectedDate);
+          console.log("Fetched daySheets:", daySheets);
+
+          const formatDate = (date: Date) => {
+            const year = date.getFullYear();
+            const month = (date.getMonth() + 1).toString().padStart(2, "0");
+            const day = date.getDate().toString().padStart(2, "0");
+            return `${year}-${month}-${day}`;
+          };
+
+          const selectedDateString = formatDate(formattedSelectedDate);
+
+          let matchingDaySheet: DaySheetDto | undefined;
+
+          daySheets.forEach((daySheet) => {
+            const daySheetDateString = formatDate(
+              new Date(daySheet.date ? daySheet.date : new Date())
+            );
+            console.log("DaySheet date:", daySheetDateString);
+
+            if (daySheetDateString === selectedDateString) {
+              console.log("Found matching daySheet:", daySheet);
+              matchingDaySheet = daySheet;
+            }
+          });
+
+          if (matchingDaySheet) {
+            setDaySheetId(matchingDaySheet.id || null);
+            setRatings(matchingDaySheet.moodRatings || []);
+          } else {
+            setDaySheetId(null);
+            setRatings([]);
+          }
+
+          console.log("Fetched daySheetId:", matchingDaySheet?.id);
+        }
       } catch (error) {
-        console.error("Failed to fetch daySheet", error);
+        toast.error("Failed to fetch daySheet");
       }
     };
     fetchDaySheet();
-  }, [selectedDate]);
+  }, [selectedDate, selectedParticipant]);
 
   useEffect(() => {
     if (user) {
@@ -107,20 +186,31 @@ const MoodTrackingPage = () => {
   }, [user]);
 
   useEffect(() => {
-    fetchRatings();
+    //fetchRatings();
   }, [selectedDate, selectedParticipant, role]);
 
   const fetchUserRole = async () => {
     try {
       const userApi = getUserControllerApi();
-      const userDto = await userApi.getUserById({ id: user.sub });
+      const userDto = await userApi.getUserById({
+        id: user?.sub ?? "",
+      });
       setRole(userDto.role || "");
       if (userDto.role === "SOCIAL_WORKER" || userDto.role === "ADMIN") {
-        const users = await userApi.getAllUsers();
+        const users = await userApi.getAllParticipants();
         setParticipants(users);
+        //first returned user to be set as selected participant
+        if (users.length > 0 && users[0]?.userId !== undefined) {
+          setSelectedParticipant(users[0].userId);
+          fetchCategories(users[0].userId);
+        } else {
+          setSelectedParticipant("");
+        }
       } else if (userDto.role === "PARTICIPANT") {
         const categoryApi = getCategoryControllerApi();
-        const categories = await categoryApi.getCategoryListByUserId({ userId: user.sub ?? "" });
+        const categories = await categoryApi.getCategoryListByUserId({
+          userId: user?.sub ?? "",
+        });
         setCategories(categories);
       }
       console.log("Fetched user role:", userDto.role);
@@ -130,32 +220,46 @@ const MoodTrackingPage = () => {
   };
 
   const fetchRatings = async () => {
-    const ratingApi = getRatingControllerApi();
+    const daySheetApi = getDaySheetControllerApi();
     try {
-      const ratings = await ratingApi.getMoodRatingByDate({
-        date: selectedDate,
-        userId: role === "PARTICIPANT" ? user?.sub : selectedParticipant !== allParticipants ? selectedParticipant : undefined,
-      });
-
-      const formattedRatings = ratings.map(rating => ({
-        category: rating.rating.category.name.trim(),
-        participantName: rating.participantName,
-        rating: rating.rating.rating
-      }));
-
-      setRatings(formattedRatings);
-      console.log("Fetched ratings:", formattedRatings);
+      if (role === "PARTICIPANT") {
+        const daySheet = await daySheetApi.getDaySheetDate({
+          date: selectedDate,
+        });
+        setRatings(daySheet?.moodRatings || []);
+      } else {
+        const daySheets = await daySheetApi.getAllDaySheetByParticipantAndMonth(
+          {
+            userId: selectedParticipant,
+            month: selectedDate.slice(0, 7),
+          }
+        );
+        const formattedSelectedDate = new Date(selectedDate.slice(0, 10))
+          .toISOString()
+          .split("T")[0];
+        const daySheet = daySheets.find(
+          (sheet) =>
+            sheet.date?.toISOString().split("T")[0] === formattedSelectedDate
+        );
+        setRatings(daySheet?.moodRatings || []);
+      }
     } catch (error) {
       console.error("Failed to fetch ratings", error);
     }
   };
 
-  const fetchCategories = async (participantId) => {
+  const fetchCategories = async (participantId: string | undefined) => {
     const categoryApi = getCategoryControllerApi();
     try {
-      const categories = await categoryApi.getCategoryListByUserId({ userId: participantId });
+      const categories = await categoryApi.getCategoryListByUserId({
+        userId: participantId || "",
+      });
       setCategories(categories);
-      console.log("Fetched categories for participant:", participantId, categories);
+      console.log(
+        "Fetched categories for participant:",
+        participantId,
+        categories
+      );
     } catch (error) {
       console.error("Failed to fetch categories", error);
     }
@@ -168,15 +272,21 @@ const MoodTrackingPage = () => {
         console.log("daySheetId", daySheetId);
         console.log("Original ratings", ratings);
 
-        const createRatingDtos = ratings.map((rating) => ({
-          categoryId: rating.category.id,
-          rating: rating.rating,
-          ratingRole: rating.ratingRole,
-        }));
+        const createRatingDtos = ratings.map(
+          (rating: {
+            category: { id: any };
+            rating: any;
+            ratingRole: any;
+          }) => ({
+            categoryId: rating.category.id,
+            rating: rating.rating,
+            ratingRole: rating.ratingRole,
+          })
+        );
 
         console.log("Transformed createRatingDtos", createRatingDtos);
         await ratingApi.createRatingsByDaySheetId({
-          daySheetId: daySheetId,
+          daySheetId: daySheetId ?? 0,
           createRatingDto: createRatingDtos,
         });
         fetchRatings();
@@ -187,13 +297,25 @@ const MoodTrackingPage = () => {
     [daySheetId]
   );
 
-  const handleDateChange = (event) => {
+  const handleDateChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
     setSelectedDate(event.target.value);
   };
 
-  const handleParticipantChange = async (participantId) => {
-    setSelectedParticipant(participantId);
-    await fetchCategories(participantId);
+  const handleParticipantChange = async (
+    participantId: SetStateAction<string>
+  ) => {
+    if (typeof participantId === "function") {
+      setSelectedParticipant((prevState) => {
+        const newParticipantId = participantId(prevState);
+        fetchCategories(newParticipantId);
+        return newParticipantId;
+      });
+    } else {
+      setSelectedParticipant(participantId);
+      await fetchCategories(participantId);
+    }
   };
 
   const participantsData = participants.map((participant) => ({
@@ -208,7 +330,11 @@ const MoodTrackingPage = () => {
           close={() => setShowModal(false)}
           onSave={handleSaveRating}
           categories={categories}
-          participants={role === "SOCIAL_WORKER" || role === "ADMIN" ? participantsData : undefined}
+          participants={
+            role === "SOCIAL_WORKER" || role === "ADMIN"
+              ? participantsData
+              : undefined
+          }
           selectedParticipant={selectedParticipant}
           setSelectedParticipant={setSelectedParticipant}
           role={role}
@@ -233,7 +359,6 @@ const MoodTrackingPage = () => {
               data={participantsData}
               value={selectedParticipant}
               onChange={(event) => handleParticipantChange(event.target.value)}
-              disabled={role === "PARTICIPANT"}
             />
           ) : null}
           <Input
@@ -244,7 +369,11 @@ const MoodTrackingPage = () => {
           />
         </div>
         <Table
-          data={ratings}
+          data={ratings.map((rating) => ({
+            category: rating.category?.name?.trim() ?? "",
+            participantName: rating.ratingRole,
+            rating: rating.rating,
+          }))}
           columns={[
             { header: "Kategorie", title: "category" },
             { header: "Teilnehmer", title: "participantName" },

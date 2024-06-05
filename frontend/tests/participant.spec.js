b378787714bf6bb50b22937dbddf92f4e0ea47d9
brunndar <brunndar@students.zhaw.ch>
2024-06-05 18:19:56 +0200

    #134 updated e2e tests for participant and social worker

--- frontend/tests/participant.spec.js
+++ frontend/tests/participant.spec.js
@@ -9,14 +9,14 @@ test.beforeEach('login charles leclerc (participant)', async ({ page }) => {
   await page.getByLabel('Password*').click();
   await page.getByLabel('Password*').fill('Test123$');
   await page.getByRole('button', { name: 'Continue' }).click();
+  await page.waitForTimeout(3000);
 });
 
 test('testing crud for timestamp', async ({ page }) => {
   //create valid timestamp
-  await page.waitForTimeout(3000);
   await page.getByRole('img').nth(1).click();
   await page.waitForTimeout(3000);
-  await page.locator('input[name="date"]').fill('9999-01-01');
+  await page.locator('input[name="date"]').fill('2024-01-01');
   await expect(page.getByRole('cell', { name: 'Keine Daten erfasst' })).toBeVisible();
   await expect(page.getByRole('cell', { name: '0h 0min' })).toBeVisible();
   await page.locator('input[name="startTime"]').click();
@@ -44,7 +44,7 @@ test('testing crud for timestamp', async ({ page }) => {
   await expect(page.getByText('Zeiteintrag konnte nicht')).toBeVisible();
   await page.waitForTimeout(3000);
 
-  //change timestamp
+  //update timestamp
   await page.getByRole('row', { name: ':00 17:00 9h 0min' }).getByRole('button').nth(1).click();
   await expect(page.locator('h2')).toContainText('Zeiteintrag bearbeiten');
   await expect(page.locator('form').filter({ hasText: 'Zeiteintrag' }).locator('input[name="startTime"]')).toHaveValue('08:00');
@@ -62,5 +62,15 @@ test('testing crud for timestamp', async ({ page }) => {
 
   //delete timestamp
   await page.getByRole('row', { name: ':15 17:30 9h 15min' }).getByRole('button').first().click();
-  
+
+});
+
+test('testing crud for moods', async ({ page }) => {
+  await page.getByText('Stimmung erfassen').click();
+  await page.getByRole('textbox').fill('2024-01-01');
+  await expect(page.locator('td')).toContainText('Keine Daten erfasst');
+  await page.getByRole('button', { name: 'Rating abgeben' }).click();
+  await expect(page.locator('h2')).toContainText('Rating abgeben');
+  await expect(page.getByRole('button', { name: 'Speichern' })).toBeVisible();
+  await page.locator('form div').filter({ hasText: 'Rating abgeben' }).getByRole('button').click();
 });

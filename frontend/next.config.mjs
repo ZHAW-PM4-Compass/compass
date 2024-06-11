f377ff2d21d3c94ce12c9de3e03e92e6aef20e2d
r44cx <29802889+r44cx@users.noreply.github.com>
2024-06-11 20:25:55 +0200

    remove sentry from project

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,5 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { withSentryConfig } from '@sentry/nextjs';
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,52 +12,37 @@ const bundleAnalyzer = withBundleAnalyzer({
 const isTest = process.env.NODE_ENV === 'test';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withSentryConfig(
-  bundleAnalyzer({
-    eslint: {
-      dirs: ['.'],
-    },
-    poweredByHeader: false,
-    reactStrictMode: true,
-    experimental: {
-      serverComponentsExternalPackages: ['pino'],
-    },
-    webpack: (config, { dev, isServer }) => {
-      config.externals.push({
-        bufferutil: 'bufferutil',
-        'utf-8-validate': 'utf-8-validate',
-      });
+const nextConfig = bundleAnalyzer({
+  eslint: {
+    dirs: ['.'],
+  },
+  poweredByHeader: false,
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['pino'],
+  },
+  webpack: (config, { dev, isServer }) => {
+    config.externals.push({
+      bufferutil: 'bufferutil',
+      'utf-8-validate': 'utf-8-validate',
+    });
 
-      if (isTest) {
-        config.module.rules.push({
-          test: /\.(js|jsx|ts|tsx)$/,
-          include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'pages')],
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [['next/babel']],
-              plugins: ['babel-plugin-istanbul'],
-            },
+    if (isTest) {
+      config.module.rules.push({
+        test: /\.(js|jsx|ts|tsx)$/,
+        include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'pages')],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['next/babel']],
+            plugins: ['babel-plugin-istanbul'],
           },
-        });
-      }
+        },
+      });
+    }
 
-      return config;
-    },
-  }),
-  {
-    silent: true,
-    org: 'nextjs-boilerplate-org',
-    project: 'nextjs-boilerplate',
+    return config;
   },
-  {
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    tunnelRoute: '/monitoring',
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-  },
-);
+});
 
 export default nextConfig;
